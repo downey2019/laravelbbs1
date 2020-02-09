66addffb83dd8422894df9f5389134be2cf07195
vagrant <vagrant@homestead>
2020-02-09 16:43:17 +0000

    翻译URL标示

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Handlers\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -21,7 +22,13 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {   
+        //XSS
         $topic->body = clean($topic->body,'user_topic_body');
+        //话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+        
+        if(! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
